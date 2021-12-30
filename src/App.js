444f025f27cf6c7da9aa8f2eb055175f548b3f4f
rmb-repos <ranjammailbox2015@gmail.com>
2021-12-30 12:33:51 +0530

    0.0.0-1 Updated welcome page content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import React, { useState, useRef } from 'react';
-
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import { Toast } from 'primereact/toast';
-import PrimeReact from 'primereact/api';
+import React from 'react';
 
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
@@ -14,38 +9,56 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [text, setText] = useState('');
-  const toastRef = useRef();
-
-  // active ripple effect
-  PrimeReact.ripple = true;
-
-  const onFormSubmit = (e) => {
-    if (text) {
-      toastRef.current.show({ severity: 'info', summary: text, life: 3000 });
-    }
-
-    // clear
-    setText('');
-
-    e.preventDefault();
-  }
 
   return (
     <div className="App">
-
-      <Toast ref={toastRef} />
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-
-      <form className="p-d-flex p-jc-center p-mt-6" onSubmit={onFormSubmit}>
-        <InputText value={text} onChange={(e) => setText(e.target.value)} />
-        <Button type="submit" label="Submit" icon="pi pi-check" className="p-ml-2"/>
-      </form>
-    </div>
+      <div className="container">
+        <header className="header">
+          <h1 className="mt-0">Hello Developer!</h1>
+          <p>Welcome to Favstores onboarding template</p>
+        </header>
+        <div className="content">
+          <div className="card w-12">
+            <div className="flex flex-row align-items-center justify-content-center flex-wrap card-container blue-container">
+              <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-blue-500 text-white border-round m-2">1</div>
+              <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-blue-500 text-white border-round m-2">2</div>
+              <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-blue-500 text-white border-round m-2">3</div>
+            </div>
+          </div>
+          <h2 className="text-center w-12">Layout introduced by Prime Flex</h2>
+          <h3 className="text-center w-12"><a href="https://www.primefaces.org/primeflex/" alt="prime-flex-css-library">https://www.primefaces.org/primeflex/</a></h3>
+          <div className="card w-12">
+            <div className="flex card-container blue-container">
+              <div className="flex-grow-1 flex-shrink-1 flex align-items-center justify-content-center bg-blue-500 font-bold text-white p-4 m-3 border-round">
+              Note: style should be given in javascript format as  minWidth: 200, minHeight: 50 in double curly braces
+              </div>
+              <div className="flex-shrink-0 flex align-items-center justify-content-center bg-blue-500 font-bold text-white p-4 m-3 border-round">shrink item</div>
+              <div className="flex-grow-1 flex-shrink-1 flex align-items-center justify-content-center bg-blue-500 font-bold text-white p-4 m-3 border-round">
+                Note: class should be given as className
+              </div>
+            </div>
+          </div>
+          <p>Edit src/app.js to make your changes</p>
+          <p>Use npm i && npm start in repo through cmd / terminal to load webpage in your browser</p>
+          <div className="card">
+            <div className="flex flex-wrap card-container yellow-container">
+              <div className="md:flex-order-2 flex align-items-center justify-content-center w-4rem h-4rem bg-yellow-500 font-bold text-gray-900 border-round m-2">1</div>
+              <div className="md:flex-order-1 flex align-items-center justify-content-center w-4rem h-4rem bg-yellow-500 font-bold text-gray-900 border-round m-2">2</div>
+              <div className="md:flex-order-0 flex align-items-center justify-content-center w-4rem h-4rem bg-yellow-500 font-bold text-gray-900 border-round m-2">3</div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="flex align-content-around flex-wrap card-container indigo-container" style={{"minWeight": 320, "maxWidth": 700}}>
+              <div className="flex align-items-center justify-content-center bg-indigo-500 font-bold text-white border-round m-2" style={{"minWidth": 200, "minHeight": 50}}>1</div>
+              <div className="flex align-items-center justify-content-center bg-indigo-500 font-bold text-white border-round m-2" style={{"minWidth": 200, "minHeight": 50}}>2</div>
+              <div className="flex align-items-center justify-content-center bg-indigo-500 font-bold text-white border-round m-2" style={{"minWidth": 200, "minHeight": 50}}>3</div>
+              <div className="flex align-items-center justify-content-center bg-indigo-500 font-bold text-white border-round m-2" style={{"minWidth": 200, "minHeight": 50}}>4</div>
+              <div className="flex align-items-center justify-content-center bg-indigo-500 font-bold text-white border-round m-2" style={{"minWidth": 200, "minHeight": 50}}>5</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
-
 export default App;
